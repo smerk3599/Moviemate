90c77476b7419a1ffc450c24b0eb9c514304b825
Steve Mercer <stevenmercer9913@gmail.com>
2024-01-11 12:55:16 -0800

    Header/Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { AllRoutes } from "./routes/AllRoutes";
+import { Header, Footer } from "./components";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <AllRoutes />
+      <Footer />
     </div>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 export const MovieList = () => {
   return (
-    <div className="text-9xl bg-emerald-200 p-10 border-2 border-blue-700 rounded-lg m-5">
+    <div className=" text-center m-5">
       <main>MovieList</main>
     </div>
   );
